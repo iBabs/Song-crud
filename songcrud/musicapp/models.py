3139785347f7af2c5ffbd432a277a6aa4638ed12
iBabs <ibrahimbabalola8@gmail.com>
2022-10-27 10:58:18 +0100

    added song and artist

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Artist(models.Model):
+    first_name= models.CharField(max_length = 25)
+    last_name = models.CharField(max_length = 25)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return  (self.first_name)
+
+
+class Song(models.Model):
+    title = models.CharField(max_length = 25)
+    artist_id = models.ForeignKey(Artist, on_delete = models.CASCADE)
+    date_released = models.DateTimeField
+    likes = models.IntegerField
+
+    def __str__(self):
+        return  (self.title)
+
+class Lyrics(models.Model):
+    song_id = models.ForeignKey(Song, on_delete = models.CASCADE)
+    content = models.TextField()
+
+    def __str__(self):
+        return  (self.content)
+
+
+
