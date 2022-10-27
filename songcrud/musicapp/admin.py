563ba84ffce18d4702596f47106081ff478fc287
iBabs <ibrahimbabalola8@gmail.com>
2022-10-27 11:18:30 +0100

    updated admin

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 from .models import Artist, Song, Lyrics
 
 # Register your models here.
-admin.site.register(Artist)
-admin.site.register(Song)
+
+
+
 admin.site.register(Lyrics)
+
+@admin.register(Artist)
+class  ArtistAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'age', )
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):
+    list_display = ('artist_id', 'date_released')
