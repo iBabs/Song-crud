f8ba5b07fbebffb18e1744c82ebc5b70868f8f02
iBabs <ibrahimbabalola8@gmail.com>
2022-11-04 17:58:02 +0100

    RestAPI created and Installed

--- songcrud/musicapp/admin.py
+++ songcrud/musicapp/admin.py
@@ -4,13 +4,16 @@ from .models import Artist, Song, Lyrics
 # Register your models here.
 
 
-
-admin.site.register(Lyrics)
-
 @admin.register(Artist)
 class  ArtistAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'age', )
 
 @admin.register(Song)
 class SongAdmin(admin.ModelAdmin):
-    list_display = ('artist_id', 'date_released')
+    list_display = ('artist_id', 'title')
+
+
+@admin.register(Lyrics)
+class LyricsAdmin(admin.ModelAdmin):
+    list_display = ('song_id', 'content')
+    
--- songcrud/musicapp/views.py
+++ songcrud/musicapp/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .serializer import SongSerializer
+from .models import Song
+ 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
 
-# Create your views here.
+
+class SongViewSet(viewsets.ModelViewSet):
+    queryset = Song.objects.all()
+    serializer_class= SongSerializer
+
+
+@api_view(["GET","POST"])
+def songlist(request):
+    if request.method == "GET":
+        songs= Song.objects.all()
+        serializer = SongSerializer(songs, many =True)
+        return JsonResponse({"Songs":serializer.data})
+    
+    if request.method == "POST":
+        serializer= SongSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status = status.HTTP_201_CREATED)
+
+
